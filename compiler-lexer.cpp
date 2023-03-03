9040e6e80f52d0ee69e8ca73e7d215991941027a
BKGa <bkbgatchalian@csu.fullerton.edu>
2023-03-03 02:07:57 -0800

    Second Commit

--- compiler-lexer.cpp
+++ compiler-lexer.cpp
@@ -4,12 +4,52 @@
 #include <fstream>
 
 using namespace std;
+
+bool isSingleDigit(string current) {
+  if (current == "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
+    return true;
+  }
+  return false;
+}
+
+bool isKeyword(string st) {
+  if (st == "int" || st == "function" || st == "int" || st == "bool" || st == "real" || st == "if" ||
+      st == "else" || st == "fi" || st == "return" || st == "put" || st == "get" || st == "while"
+      || st == "endwhile" || st == "true" || st == "false") {
+        return true;
+      }
+  return false;
+}
+
+bool isOperator(char current) {
+  if (current == '=' || current == '!' || current == '>' || current == '<' || current == '*' ||
+      current == '/' || current == '-' || current == '+') {
+        return true;
+      }
+  return false;
+}
+
+bool isLogicalOperator(char current, char next){
+  if ((current == '<' || current == '>' || current == '=') && (next == '=')) {
+    return true;
+  }
+  return false;
+}
+
+bool isSeparator(char current){
+  if (current == '(' || current == ')' || current == '{' || current == '}' || current == ';' || current == ':') {
+    return true;
+  }
+  return false;
+}
+
 void lexer(string input){
 
     string current_token = "";
     string current_type = "";
     for (size_t i = 0; i < input.length(); i++) {
         char current_char = input[i];
+        char next_char = input[i+1];
 
         if (isspace(current_char)) {
             continue;
@@ -19,9 +59,7 @@ void lexer(string input){
             while (isalnum(input[i + 1])) {
                 current_token += input[++i];
             }
-            if (current_token == "int" || current_token == "function" || current_token == "int" || current_token == "bool" || current_token == "real" || current_token == "if" ||
-                current_token == "else" || current_token == "fi" || current_token == "return" || current_token == "put" || current_token == "get" || current_token == "while"
-                || current_token == "endwhile" || current_token == "true" || current_token == "false") {
+            if (isKeyword(current_token) == true) {
                 current_type = current_token;
                 cout << "Keyword         " << current_token << endl;
             }
@@ -31,23 +69,38 @@ void lexer(string input){
             current_token = "";
         }
         else if (isdigit(current_char) || current_char == '.') {
+            int decimals;
             current_token += current_char;
+            if (current_char == '.') {
+              decimals++;
+            }
             while (isdigit(input[i + 1]) || input[i + 1] == '.') {
                 current_token += input[++i];
+                if (input[i+1] == '.') {
+                  decimals++;
+                }
             }
-            if (current_token == "0" || "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9") {
-                cout << "Integer         " << current_token << endl;
-            }
-            else{
+            if (decimals == 1) {
                 cout << "Real            " << current_token << endl;
+            }
+            else if (isSingleDigit(current_token) == true){
+                cout << "Integer         " << current_token << endl;
             }
             current_token = "";
         }
-        else if (current_char == '=' || current_char == '!' || current_char == '>' || current_char == '<' || current_char == '*' ||
-            current_char == '/' || current_char == '-' || current_char == '+') {
+        else if (isOperator(current_char) == true) {
+          if (isLogicalOperator(current_char, next_char) == true) {
+            current_token += current_char;
+            current_token += next_char;
+            cout << "Operator        " << current_token << endl;
+            i++;
+          }
+          else {
             cout << "Operator        " << current_char << endl;
+          }
+          current_token = "";
         }
-        else if (current_char == '(' || current_char == ')' || current_char == '{' || current_char == '}' || current_char == ';' || current_char == ':') {
+        else if (isSeparator(current_char) == true) {
             cout << "Separator       " << current_char << endl;
         }
         else if (current_char == '[') {
@@ -85,16 +138,6 @@ int main(int argc, char* argv[]) {
     /* while not finished(i.e. not end of the source file) do
         call the lexer for a token
         print the tokenand lexeme
-        endwhile *\
-    }
+        endwhile
 
-
-
-//Old Main
-/*
-int main()
-{
-    char c[100] = "int m = n + 3p";
-    char d[100] = "while  (fahr <= upper)   a = 23.00; endwhile";
-    return 0;
-} */
+    }*/
